3166a0a09a6a88e82418a3f1ea5b9a99e4fb30b4
Stepan Kutuzov <stepan.kutuzovx27@gmail.com>
2023-03-16 19:38:02 +0300

    information to admin about Errors

--- send_emails.py
+++ send_emails.py
@@ -1,5 +1,6 @@
 import os, sys
 import django
+import datetime
 from django.core.mail import EmailMultiAlternatives
 
 from django.contrib.auth import get_user_model
@@ -9,12 +10,17 @@ sys.path.append(proj)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'scraping_service.settings'
 
 django.setup()
-from scraping.models import Vacancy
+from scraping.models import Vacancy, Error
 from scraping_service.settings import EMAIL_HOST_USER
-subject = "Рассылка вакансий "
-text_content = "Рассылка вакансий "
+ADMIN_USER = EMAIL_HOST_USER
+
+today = datetime.date.today()
+subject = f"Рассылка вакансий за {today} "
+text_content = f"Рассылка вакансий {today}"
 from_email = EMAIL_HOST_USER
 empty = '<h2>К сожалению на сегодня вашим предпочтения данных нет</h2>'
+
+
 User = get_user_model()
 qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
 users_dct = {}
@@ -26,7 +32,7 @@ if users_dct:
     for pair in users_dct.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params).values()[:10]
+    qs = Vacancy.objects.filter(**params, timestamp=today).values()
     vacancies = {}
     for i in qs:
         vacancies.setdefault((i['city_id'], i['language_id']), [])
@@ -41,9 +47,26 @@ if users_dct:
         _html = html if html else empty
         for email in emails:
             to = email
-            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg = EmailMultiAlternatives(
+                subject, text_content, from_email, [to]
+            )
             msg.attach_alternative(_html, "text/html")
             msg.send()
+qs = Error.objects.filter(timestamp=today)
+if qs.exists():
+    error = qs.first()
+    data = error.data
+    _html = ''
+    for i in data:
+        _html += f'<p"><a href="{i["url"]}">Error:{i["title"]}</a></p>'
+    subject = ""
+    text_content = ""
+    to = ADMIN_USER
+    msg = EmailMultiAlternatives(
+        subject, text_content, from_email, [to]
+    )
+    msg.attach_alternative(_html, "text/html")
+    msg.send()
 
 
 # subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
