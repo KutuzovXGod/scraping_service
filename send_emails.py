072b21585927c706545fe71a34207cd623827157
Stepan Kutuzov <stepan.kutuzovx27@gmail.com>
2023-03-17 18:00:04 +0300

    Searching for unknown(error) urls to admin

--- send_emails.py
+++ send_emails.py
@@ -1,3 +1,5 @@
+
+
 import os, sys
 import django
 import datetime
@@ -7,25 +9,28 @@ from django.contrib.auth import get_user_model
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'scraping_service.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
-from scraping.models import Vacancy, Error
-from scraping_service.settings import EMAIL_HOST_USER
+from scraping.models import Vacancy, Error, Url
+from scraping_service.settings import (
+    EMAIL_HOST_USER,
+    EMAIL_HOST, EMAIL_HOST_PASSWORD
+)
+
 ADMIN_USER = EMAIL_HOST_USER
 
 today = datetime.date.today()
-subject = f"Рассылка вакансий за {today} "
+subject = f"Рассылка вакансий за {today}"
 text_content = f"Рассылка вакансий {today}"
 from_email = EMAIL_HOST_USER
-empty = '<h2>К сожалению на сегодня вашим предпочтения данных нет</h2>'
-
+empty = '<h2>К сожалению на сегодня по Вашим предпочтениям данных нет. </h2>'
 
 User = get_user_model()
 qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
 users_dct = {}
 for i in qs:
-    users_dct.setdefault((i['city'], i['language']), []) # quer
+    users_dct.setdefault((i['city'], i['language']), [])
     users_dct[(i['city'], i['language'])].append(i['email'])
 if users_dct:
     params = {'city_id__in': [], 'language_id__in': []}
@@ -41,7 +46,7 @@ if users_dct:
         rows = vacancies.get(keys, [])
         html = ''
         for row in rows:
-            html += f'<h5"><a href="{row["url"]}">{row["title"]}</a></h5>'
+            html += f'<h3"><a href="{ row["url"] }">{ row["title"] }</a></h3>'
             html += f'<p>{row["description"]} </p>'
             html += f'<p>{row["company"]} </p><br><hr>'
         _html = html if html else empty
@@ -52,26 +57,52 @@ if users_dct:
             )
             msg.attach_alternative(_html, "text/html")
             msg.send()
+
 qs = Error.objects.filter(timestamp=today)
+subject = ''
+text_content = ''
+to = ADMIN_USER
+_html = ''
 if qs.exists():
     error = qs.first()
     data = error.data
-    _html = ''
     for i in data:
-        _html += f'<p"><a href="{i["url"]}">Error:{i["title"]}</a></p>'
-    subject = ""
-    text_content = ""
-    to = ADMIN_USER
-    msg = EmailMultiAlternatives(
-        subject, text_content, from_email, [to]
-    )
+        _html += f'<p"><a href="{ i["url"] }">Error: { i["title"] }</a></p><br>'
+    subject += f"Ошибки скрапинга {today}"
+    text_content += "Ошибки скрапинга"
+
+qs = Url.objects.all().values('city', 'language')
+urls_dct = {(i['city'], i['language']): True for i in qs}
+urls_err = ''
+for keys in users_dct.keys():
+    if keys not in urls_dct:
+        urls_err += f'<p"> Для города: {keys[0]} и ЯП: {keys[1]} отсутствуют урлы</p><br>'
+if urls_err:
+    subject += ' Отсутствующие урлы'
+    _html += urls_err
+
+if subject:
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(_html, "text/html")
     msg.send()
 
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+#
+# msg = MIMEMultipart('alternative')
+# msg['Subject'] = 'Список вакансий за  {}'.format(today)
+# msg['From'] = EMAIL_HOST_USER
+# mail = smtplib.SMTP()
+# mail.connect(EMAIL_HOST, 25)
+# mail.ehlo()
+# mail.starttls()
+# mail.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+#
+# html_m = "<h1>Hello world</h1>"
+# part = MIMEText(html_m, 'html')
+# msg.attach(part)
+# mail.sendmail(EMAIL_HOST_USER, [to], msg.as_string())
+# mail.quit()
 
-# subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
-# text_content = 'This is an important message.'
-# html_content = '<p>This is an <strong>important</strong> message.</p>'
-# msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-# msg.attach_alternative(html_content, "text/html")
-# msg.send()
+
