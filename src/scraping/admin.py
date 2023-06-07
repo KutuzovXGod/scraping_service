0057cefef6e55101abaec45621639405dd4a62b3
Stepan Kutuzov <stepan.kutuzovx27@gmail.com>
2023-06-07 16:51:09 +0300

    deleted everything to fix git

--- src/scraping/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import City, Language, Vacancy, Error, Url
-
-admin.site.register(Language)
-admin.site.register(City)
-admin.site.register(Vacancy)
-admin.site.register(Error)
-admin.site.register(Url)
